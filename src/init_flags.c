559044aeee295b4428a7ed18443f00d4fc589c62
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-03-09 16:04:46 +0100

    [ADD] commit by Etienne, function for attribution of values to the flags done

---
+++ src/init_flags.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2019
+** Visual Studio Live Share (Workspace)
+** File description:
+** init_flags.c
+*/
+
+#include <curses.h>
+#include "tetris.h"
+
+tetris_flags_t init_flags(void)
+{
+    tetris_flags_t flags;
+
+    flags.level = 1;
+    flags.keys[LEFT] = KEY_LEFT;
+    flags.keys[RIGHT] = KEY_RIGHT;
+    flags.keys[TURN] = KEY_UP;
+    flags.keys[DROP] = KEY_DOWN;
+    flags.keys[QUIT] = 'q';
+    flags.keys[PAUSE] = ' ';
+    flags.nb_rows = 20;
+    flags.nb_cols = 10;
+    flags.show_next = FALSE;
+    flags.debug = false;
+
+    return (flags);
+}
