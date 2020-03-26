02dd5179005b55476ca97c644f185587e9ecb374
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-03-26 18:26:56 +0100

    [RM] check_args function in main.c

--- src/main.c
+++ src/main.c
@@ -38,23 +38,6 @@ static const struct flag_association flags_list[] = {
     {'\0', NULL}
 };
 
-static bool get_long_flag_index(char const *flag, bool *require_arg)
-{
-    char long_flag[my_strlen(flag) + 1];
-    int equal_index = my_strchr_index(my_strcpy(long_flag, flag), '=');
-    int i = 0;
-
-    if (equal_index >= 0)
-        long_flag[equal_index] = '\0';
-    for (i = 0; long_opt[i].name != NULL; i += 1) {
-        if (my_strcmp(long_opt[i].name, long_flag) == 0) {
-            *require_arg = long_opt[i].has_arg;
-            return (true);
-        }
-    }
-    return (false);
-}
-
 static bool get_flag_index(char flag, int *flag_index)
 {
     int i = 0;
@@ -66,26 +49,6 @@ static bool get_flag_index(char flag, int *flag_index)
         }
     }
     return (false);
-}
-
-static bool check_args(int ac, char **av)
-{
-    int i = 0;
-    bool require_arg = false;
-
-    for (i = 1; i < ac; i += 1) {
-        if (av[i][0] == '-' && av[i][1] != '-' && my_strlen(&av[i][1]) > 1)
-            return (false);
-        if (my_strncmp(av[i], "--", 2) != 0)
-            continue;
-        if (!get_long_flag_index(&av[i][2], &require_arg)) {
-            my_printf("%s: unrecognized option '%s'\n", av[0], av[i]);
-            return (false);
-        }
-        if (require_arg && !my_strchr(av[i], '='))
-            return (false);
-    }
-    return (true);
 }
 
 static bool parse_args(int ac, char **av, tetris_t *tetris)
@@ -109,8 +72,6 @@ int main(int ac, char **av)
 {
     tetris_t tetris = init_tetris();
 
-    if (check_args(ac, av) == false)
-        return (84);
     if (parse_args(ac, av, &tetris) == false)
         return (84);
     return (tetris_game(tetris));
